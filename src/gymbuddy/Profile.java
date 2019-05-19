44d54df2cc9d12cd708a1ffcc5ca78163d9064e4
Justin Beringer <jbberinger@hotmail.com>
2019-05-19 13:24:42 -0700

    Update Profile.java

--- src/gymbuddy/Profile.java
+++ src/gymbuddy/Profile.java
@@ -3,11 +3,6 @@ package gymbuddy;
 import java.util.HashSet;
 import java.util.Set;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * Stores profile information
